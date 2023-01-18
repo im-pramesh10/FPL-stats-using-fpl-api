4d09ef1ae0510cb7926e531925a58486395f2600
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-18 22:11:37 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-wrapper">
+        <h1 className="title">
+          WELCOME
+        </h1>
+        <h1 className="title">
+          TO
+        </h1>
+        <h2 className="title">
+          FPL STATISTICS
+        </h2>
+      </div>
     </div>
   );
 }
