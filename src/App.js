46173bb772053e9009b142eb770168ecb6f0ea76
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-19 16:55:59 +0545

    90% skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
+import Search from './components/search';
+import SearchDisplay from './components/searchDisplay';
 
 function App() {
   return (
-    <div className="App">
-      <div className="header-wrapper">
-        <h1 className="title">
-          WELCOME
-        </h1>
-        <h1 className="title">
-          TO
-        </h1>
-        <h2 className="title">
-          FPL STATISTICS
-        </h2>
-      </div>
+      <div className="App">
+        <div className="header-wrapper">
+          <h1 className="title">
+            WELCOME
+          </h1>
+          <h1 className="title">
+            TO
+          </h1>
+          <h2 className="title">
+            FPL STATISTICS
+          </h2>
+        </div>
+        <Search/>
+        <SearchDisplay/>
     </div>
   );
 }
