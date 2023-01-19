3fb370d3f0e9eac89f5b0ac33b73e2c1ad89f825
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-19 16:53:40 +0545

    90% skeleton

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import "./search.css";
+
+const search = () => {
+    return (
+        <div className="search-wrapper">
+            <input type="search" id="search" placeholder="Search..." />
+        </div>    
+        );
+}
+
+export default search;
---
+++ src/components/searchDisplay.js
@@ -0,0 +1,25 @@
+import "./searchDisplay.css";
+
+const searchDisplay = ({ displayvalue }) => {
+    return(
+        <div 
+            id="search-display"
+            style={{ display: displayvalue }}
+            >
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div><div>testing</div>
+            <div>testing</div><div>testing</div><div>testing</div><div>testing</div><div>testing</div><div>testing</div><div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+            <div>testing</div>
+        </div>
+    );
+}
+
+export default searchDisplay;
