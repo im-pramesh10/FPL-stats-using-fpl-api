fd219b14cdebe7a245ea858ea8c685fdf8ebfd1d
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-22 15:32:09 +0545

    front face done

--- src/components/card.js
+++ src/components/card.js
@@ -6,38 +6,59 @@ const card = ({ player })=>{
         <div className="card-container">
         <div className="card">
         <div className="card-front">
-            <div className="row">
-                <div className="card-item">Name: { player.name }</div>
-            </div>
-            <div>
-                <div className="card-item">Team: { player.team }</div>
-            </div>
-            <div className="row">
-                { (player.status==="a")&&<div className="card-item">Status: <span style={{ color: "white", background: "green", padding: "2px", borderRadius: "6px" }}>Available</span></div>}
-                { (player.status!=="a")&&<div className="card-item">Status: <span style={{ color: "white", background: "red", padding: "2px", borderRadius: "6px" }}>Unavailable</span></div>}
-            </div>
-            <div className="row">
-                <div className="card-item">{ player.selected_by }% Ownership</div>
-            </div>
-            {/* <div>&nbsp;</div><div>&nbsp;</div><div>&nbsp;</div><div className="info">Hover for more info</div><div>&nbsp;</div><div>&nbsp;</div><div>&nbsp;</div> */}
-            {/* <div className="card"></div> */}
-            <div className="row">
-                <div className="card-item">Goals: {player.goals}</div>
-                <div className="card-item">Assists: {player.assists}</div>
-            </div>
-            <div className="row">
-                <div className="card-item">xG: {player.xG}</div>
-                <div className="card-item">xA: {player.xA}</div>
-            </div>
-            <div className="row">
-                <div className="card-item">xGp90: {player.xGp90}</div>
-                <div className="card-item">xAp90: {player.xAp90}</div>
-            </div>
-            <div className="row">
-                <div className="card-item">xGIp90: {player.xGIp90}</div>
-                <div className="card-item">Position: {player.elements_type}</div>
-            </div>
+                <div>Name: { player.name }</div>
+           
+                <div>Team: { player.team }</div>
+           
+            
+                { (player.status==="a")&&<div>Status: <span style={{ color: "white", background: "green", padding: "2px", borderRadius: "6px" }}>Available</span></div>}
+                { (player.status!=="a")&&<div>Status: <span style={{ color: "white", background: "red", padding: "2px", borderRadius: "6px" }}>Unavailable</span></div>}
+            
+            
+                <div>{ player.selected_by }% Ownership</div>
+        
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Goals</td>
+                        <td>{player.goals}</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Assists</td>
+                        <td>{player.assists}</td>
+                    </tr>
+                    <tr>
+                        <td>xG</td>
+                        <td>{player.xG}</td>
+                    </tr>
+                    <tr>
+                        <td>xA</td>
+                        <td>{player.xA}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>xGp90</td>
+                        <td>{player.xGp90}</td>
+                    </tr>
+                    <tr>
+                        <td>xAp90</td>
+                        <td>{player.xAp90}</td>
+                    </tr>
+                    <tr>
+                        <td>xGI90</td>
+                        <td>{player.xGIp90}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                {/* <div>Position: {player.elements_type}</div> */}
+           
             <div className="info">Hover for more info</div>
+           
             </div>
             <div className="card-back">
                 hello
