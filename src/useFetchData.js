579fda768877c9ad3f4dfd360c669d2fe3eda5d6
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-25 15:04:14 +0545

    new App.js

--- src/useFetchData.js
+++ src/useFetchData.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
 export function useFetchData(url) {
-    const [data,setData] = useState[null];
-    const [error,setError] = useState[null];
+    console.log("hello");
+    const [data,setData] = useState(null);
+    const [error,setError] = useState(null);
+    const [loading,setLoading] = useState(true);
 
     async function fetchData(){
-
+        console.log("inside fetch");
+        setLoading(true);
         try {
             const response = await fetch(url);
-            const json = response.json();
+            const json = await response.json();
             setData(json);
         } catch (err) {
             setError(err);
         }
+        setLoading(false);
     }
 
 
