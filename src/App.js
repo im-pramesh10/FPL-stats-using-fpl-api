ea3c5a6ccb2a6d05f74521d381b61db8dba98978
Pramesh Mahat <cruisepramesh@gmail.com>
2023-02-01 21:51:48 +0545

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./pages/home";
 import { Route, Routes  } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import './App.css';
 
 export default function App(){
   return (
+<>
+    <NavBar/>
+   
     <Routes>
       <Route path="/" element={<Home/>} />
     </Routes>
+</>
   );
 }
